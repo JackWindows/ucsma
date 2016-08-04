b01f8fa2a47b4d657bd2dac7208c2638d5ba5590
JackWindows <fhb1992@gmail.com>
2016-08-04 17:47:33 -0400

    move include to gpio_api.c

--- gpio_api.h
+++ gpio_api.h
@@ -1,16 +1,18 @@
 #ifndef GPIO_API_
 #define GPIO_API_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-
 void _gpioExport(int);
 void _gpioUnexport(int);
 void _gpioDirection(int, int);
 void _gpioSet(int, int);
 int _gpioGet(int);
+
+
+extern volatile unsigned long *gpio_address;
+int gpioSetup();
+void gpioDirection(int, int);
+void gpioSet(int, int);
+void gpioSet2(int, int);
+int gpioGet(int);
 
 #endif
