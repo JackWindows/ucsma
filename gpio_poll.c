34784ccfaff8c8d5c615c39fd717f3e3b417898f
JackWindows <fhb1992@gmail.com>
2016-08-04 17:51:44 -0400

    remove array variable, now can set step count as high as possible

--- gpio_poll.c
+++ gpio_poll.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 #include "gpio_api.h"
 
 #define GPIO_INPUT 19
-#define MAX_STEP 100000
+#define MAX_STEP 10000000
 int main() {
-	int values[MAX_STEP];
-	int value;
+	int value, ret, count = 0, max_count = 0;
+
+	ret = gpioSetup();
+	if (ret == -1) {
+		printf("open /dev/mem failed\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (ret == -2) {
+		printf("MAP_FAILED\n");
+		exit(EXIT_FAILURE);
+	}
 
 	clock_t begin = clock();
 	for (int i = 0; i < MAX_STEP; i++) {
-		value = _gpioGet(GPIO_INPUT);
-		values[i] = value;
+		value = gpioGet(GPIO_INPUT);
+		if (value == 1) {
+			count++;
+		}
+		else {
+			if (count > 0) {
+				printf("%d consecutive high voltage detected\n", count);
+				if (count > max_count) {
+					max_count = count;
+				}
+				count = 0;
+			}
+		}
 	}
 	clock_t end = clock();
 
-	int cur_state = 0, count = 0;
-	for (int i = 0; i < MAX_STEP; i++) {
-		if (values[i] == 0) {
-			if (cur_state == 1) {
-				printf("%d consecutive high voltage detected\n", count);
-				cur_state = 0, count = 0;
-			}
-		}
-		else {
-			if (cur_state == 0) {
-				cur_state = 1;
-				count++;
-			}
-			else {
-				count++;
-			}
-		}
-	}
-	printf("\n");
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("\n");
+	printf("longest consectuive high voltage: %d\n", max_count);
 	printf("total running time for %d rounds of polling: %fs\n", MAX_STEP, time_spent);
 	return 0;
 }
