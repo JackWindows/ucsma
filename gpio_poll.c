9d0c49b1b1788a73fea7bc78ef5240c58260dc39
JackWindows <fhb1992@gmail.com>
2016-08-22 19:01:45 -0400

    modify gpio_poll to make INPUT_PIN configuarable

--- gpio_poll.c
+++ gpio_poll.c
@@ -4,8 +4,16 @@
 #include "gpio_api.h"
 
 #define GPIO_INPUT 19
-int main() {
+int main(int argc, char *argv[]) {
 	int value, last_value, ret, step = 0, count = 0; 
+	int gpio_input_pin = GPIO_INPUT;
+
+	if (argc > 1)
+		gpio_input_pin = atoi(argv[1]);
+	else {
+		fprintf(stderr, "Usage: %s <gpio input pin>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
 	ret = gpioSetup();
 	if (ret == -1) {
@@ -17,12 +25,13 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
+	gpioDirection(gpio_input_pin, 0);
 	clock_t end, begin = clock();
-	last_value = gpioGet(GPIO_INPUT);
+	last_value = gpioGet(gpio_input_pin);
 	while (1) {
-		value = gpioGet(GPIO_INPUT);
+		value = gpioGet(gpio_input_pin);
 		if (value != last_value) {
-			if (value == 0) {
+			if (value == 0 && count > 300) {
 				end = clock();
 				printf("value changed from %d to %d after %4d count at %f, step %6d\n", last_value, value, count, (double)(end - begin) / CLOCKS_PER_SEC, step);
 			}
